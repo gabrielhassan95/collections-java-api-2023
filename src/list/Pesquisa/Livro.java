4d9b6638a0cb5acfc45651ce7eae99ac51944bd5
gabrielhassan95 <ghsammour01@gmai.com>
2024-08-04 09:37:53 -0300

    Resolvendo desafio ListaTarefa
    Signed-off-by: gabrielhassan95 <ghsammour01@gmai.com>

---
+++ src/list/Pesquisa/Livro.java
@@ -0,0 +1,38 @@
+package list.Pesquisa;
+
+public class Livro {
+    //atributos
+
+    private String titulo;
+
+    private  String autor;
+
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
